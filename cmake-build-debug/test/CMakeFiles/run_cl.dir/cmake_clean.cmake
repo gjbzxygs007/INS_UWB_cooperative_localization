7f5a5acb9f3cabd64dd9b9f5bf9117c343ac260d
gjbzxygs007 <jiananz1@uci.edu>
2020-07-22 17:55:34 -0700

    Uploading

---
+++ cmake-build-debug/test/CMakeFiles/run_cl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../../bin/run_cl"
+  "../../bin/run_cl.pdb"
+  "CMakeFiles/run_cl.dir/run_cl.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_cl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/common_include.h
@@ -0,0 +1,11 @@
+
+#ifndef COOPERATIVE_COMMON_INCLUDE_H
+#define COOPERATIVE_COMMON_INCLUDE_H
+
+#include <Eigen/Core>
+
+
+
+
+
+#endif //COOPERATIVE_COMMON_INCLUDE_H
